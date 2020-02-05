1c48bbc96396a65c9a00f5575a4b448621e70422
dylan-cancelliere <dycancel@gmail.com>
2020-02-05 03:07:15 -0500

    Simple website that uses HTTP to get your IP

---
+++ ipaddress/src/IPAddressContainer.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+import IPAddress from "./IPAddress";
+
+var xhr;
+
+class IPAddressContainer extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ip_address: "..."
+        };
+
+        this.processRequest = this.processRequest.bind(this);
+    }
+
+    componentDidMount() {
+        xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://ipinfo.io/json", true);
+        xhr.send();
+
+        xhr.addEventListener("readystatechange", this.processRequest, false);
+    }
+
+    processRequest() {
+        console.log(xhr.readyState + ": " + xhr.status);
+        if (xhr.readyState === 4 && xhr.status === 200){
+            console.log("yiek1");
+            var response = JSON.parse(xhr.responseText);
+
+            this.setState({
+                ip_address: response.ip
+            });
+        }
+    }
+
+    render(){
+        return(
+            <IPAddress ip={this.state.ip_address}/>
+        );
+    }
+}
+
+export default IPAddressContainer;
